57998d7757358e9c2c08e80bf7e78446b681ea15
Tony Medrano <info@tonymdedrano.com>
2017-09-12 15:36:44 +0200

    Finishing...

--- src/animals/horse.ts
+++ src/animals/horse.ts
@@ -1,8 +1,10 @@
 import { Animal } from './animal'
 
 export class Horse extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 45):string {
+    constructor(name: string) {
+        super(name);
+    }
+    move(distanceInMeters = 45): string {
         console.log("Galloping...");
         return super.move(distanceInMeters);
     }
--- src/animals/snake.ts
+++ src/animals/snake.ts
@@ -1,7 +1,9 @@
 import { Animal } from './animal'
 
 export class Snake extends Animal {
-    constructor(name: string) { super(name); }
+    constructor(name: string) {
+        super(name);
+    }
     move(distanceInMeters = 5) {
         console.log("Slithering...");
         return super.move(distanceInMeters);
